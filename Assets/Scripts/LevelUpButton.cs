5fd726bb1f3dc76aa44146936275764e219b70ba
LeeJuYeol <37298646+lamerGit@users.noreply.github.com>
2022-07-15 19:45:51 +0900

    업그래이드 기반을 만듬
    업글레이드 한만큼 인게임에 적용해야됨 지금은 업그래이하는 모습만있음

--- Assets/Scripts/LevelUpButton.cs
+++ Assets/Scripts/LevelUpButton.cs
@@ -1,15 +1,157 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
 
 public class LevelUpButton : MonoBehaviour
 {
+    public Sprite[] SlotSprites;
+    public Image[] Slotimages;
+
+    int health = 0;
+    int attack = 0;
+    int exp = 0;
+
     private void Start()
     {
+        if(LoadGameData())
+        {
+            Slotimages[0].sprite = SlotSprites[health];
+            Slotimages[1].sprite = SlotSprites[attack];
+            Slotimages[2].sprite = SlotSprites[exp];
+        }else
+        {
+            for(int i=0; i < Slotimages.Length; i++)
+            {
+                Slotimages[i].sprite = SlotSprites[0];
+            }
+        }
         gameObject.SetActive(false);
     }
     public void ExitPanel()
     {
         gameObject.SetActive(false);
     }
+
+    public void SaveGameData()
+    {
+        //Debug.Log("button");
+        SaveData saveData = new SaveData();
+        saveData.HealthLevel = health;
+        saveData.AttackLevel = attack;
+        saveData.ExpLevel = exp;
+
+        string json=JsonUtility.ToJson(saveData);
+
+        string path = $"{Application.dataPath}/Save/";
+        
+        if(!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        string fullPath = $"{path}Save.json";
+        File.WriteAllText(fullPath, json);
+
+
+    }
+
+    public bool LoadGameData()
+    {
+        bool result = false;
+        string path = $"{Application.dataPath}/Save/";
+        string fullPath = $"{path}Save.json";
+        if(Directory.Exists(path) && File.Exists(fullPath))
+        {
+            string json=File.ReadAllText(fullPath);
+            SaveData saveData=JsonUtility.FromJson<SaveData>(json);
+            health = saveData.HealthLevel;
+            attack = saveData.AttackLevel;
+            exp = saveData.ExpLevel;
+            result = true;
+        }
+
+        return result;
+    }
+
+    public void SaveHealthData()
+    {
+        //Debug.Log("button");
+        health++;
+        health = Mathf.Clamp(health, 0, 3);
+        SaveData saveData = new SaveData();
+        saveData.HealthLevel = health;
+        saveData.AttackLevel = attack;
+        saveData.ExpLevel = exp;
+        Slotimages[0].sprite = SlotSprites[health];
+
+        string json = JsonUtility.ToJson(saveData);
+
+        string path = $"{Application.dataPath}/Save/";
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        string fullPath = $"{path}Save.json";
+        File.WriteAllText(fullPath, json);
+
+
+    }
+
+    public void SaveAttackData()
+    {
+        //Debug.Log("button");
+        attack++;
+        attack = Mathf.Clamp(attack, 0, 3);
+        SaveData saveData = new SaveData();
+        saveData.HealthLevel = health;
+        saveData.AttackLevel = attack;
+        saveData.ExpLevel = exp;
+        Slotimages[1].sprite = SlotSprites[attack];
+
+        string json = JsonUtility.ToJson(saveData);
+
+        string path = $"{Application.dataPath}/Save/";
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        string fullPath = $"{path}Save.json";
+        File.WriteAllText(fullPath, json);
+
+
+    }
+
+    public void SaveExpData()
+    {
+        //Debug.Log("button");
+        exp++;
+        exp = Mathf.Clamp(exp, 0, 3);
+        SaveData saveData = new SaveData();
+        saveData.HealthLevel = health;
+        saveData.AttackLevel = attack;
+        saveData.ExpLevel = exp;
+        Slotimages[2].sprite = SlotSprites[exp];
+
+        string json = JsonUtility.ToJson(saveData);
+
+        string path = $"{Application.dataPath}/Save/";
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        string fullPath = $"{path}Save.json";
+        File.WriteAllText(fullPath, json);
+
+
+    }
+
 }
---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class SaveData 
+{
+    public int HealthLevel;
+    public int AttackLevel;
+    public int ExpLevel;
+}
